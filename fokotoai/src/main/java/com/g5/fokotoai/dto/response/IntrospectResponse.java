ecf43417819f169402f8e96cfe45aa11089e2e15
ChuVV <vuvanchu3012@gmail.com>
2026-06-03 21:52:31 +0700

    Merge after TuDM refactor code

---
+++ fokotoai/src/main/java/com/g5/fokotoai/dto/response/IntrospectResponse.java
@@ -0,0 +1,13 @@
+package com.g5.fokotoai.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class IntrospectResponse {
+    boolean isTokenValid ;
+}
