285705ab243381056662fe2b1f38e213dd89cc5b
apple31279 <nanhphuong2005@gmail.com>
2026-07-19 02:40:04 +0700

    fix: Remove explicit @EnableJpaRepositories to support api-only profile

--- fokotoai/src/main/java/com/g5/fokotoai/FokotoaiApplication.java
+++ fokotoai/src/main/java/com/g5/fokotoai/FokotoaiApplication.java
@@ -2,14 +2,12 @@ package com.g5.fokotoai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
-@EnableJpaRepositories("com.g5.fokotoai.repository")
 public class FokotoaiApplication {
 
     public static void main(String[] args) {
